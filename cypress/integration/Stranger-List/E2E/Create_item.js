62e79005f96c74cf3e74f5445632c0e984d7fda7
Juan Pablo Romero <jpromero@playvox.com>
2022-06-06 17:19:11 -0500

    fix: Item deletion is moved to be at the end of execution
    - Changed the way items are removed
    - Two contexts are added, to handle when files are uploaded and when files cannot be uploaded

--- cypress/integration/Stranger-List/E2E/Create_item.js
+++ cypress/integration/Stranger-List/E2E/Create_item.js
@@ -11,134 +11,158 @@ import Chance from 'chance'
 const resolutions = ['iphone-xr', [1280, 720]]
 
 describe('Create item', () => {
-    beforeEach(function () {
-        cy.intercept({
-            method: 'GET',
-            url: '**/api/items',
-        }).as('getItems')
+    context('Create item successfully', () => {
+        beforeEach(function () {
+            cy.intercept({
+                method: 'GET',
+                url: '**/api/items',
+            }).as('getItems')
 
-        cy.intercept({
-            method: 'POST',
-            url: '**/api/items',
-        }).as('postItems')
-    })
+            cy.intercept({
+                method: 'POST',
+                url: '**/api/items',
+            }).as('postItems')
+        })
 
-    resolutions.forEach((resolution) => {
+        afterEach(function () {
+            cy.CC_delete_item(this.lasItem.idLastItem)
+        })
 
-        it(`Create item successfully with adequate image(jpg, png, gif) on ${resolution}`, function () {
-            if (Cypress._.isArray(resolution)) {
-                cy.viewport(resolution[0], resolution[1])
-            } else {
-                cy.viewport(resolution)
-            }
+        resolutions.forEach((resolution) => {
 
-            const filePath1 = 'chapter8-250px-250px.jpg'
-            let synopsisChapter8 = chance.paragraph({sentences: 1})
+            it(`Create item successfully with adequate image(jpg, png, gif) on ${resolution}`, function () {
+                if (Cypress._.isArray(resolution)) {
+                    cy.viewport(resolution[0], resolution[1])
+                } else {
+                    cy.viewport(resolution)
+                }
 
-            cy.visit('/')
-            cy.wait('@getItems')
+                const filePath1 = 'chapter8-250px-250px.jpg'
+                let synopsisChapter8 = chance.paragraph({sentences: 1})
 
-            cy.get('#inputImage').attachFile(filePath1)
-            cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8).invoke('val').as('descriptionText')
-            cy.findByRole('button', {name: /create item/i}).should('be.enabled').click()
-            cy.wait('@postItems').then((resp) => {
-                const body = resp.response.body
-                let lastItem = body[body.length - 1]
-                let idLastItem = lastItem.id
-                let descriptionLastItem = lastItem.text
+                cy.visit('/')
+                cy.wait('@getItems')
 
-                cy.log(`ID LAST ITEM: ${idLastItem}`)
-                cy.log(`DESCRIPTION LAST ITEM: ${descriptionLastItem}`)
+                cy.get('#inputImage').attachFile(filePath1)
+                cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8).invoke('val').as('descriptionText')
+                cy.findByRole('button', {name: /create item/i}).should('be.enabled').click()
+                cy.wait('@postItems').then((resp) => {
+                    const body = resp.response.body
+                    let lastItem = body[body.length - 1]
+                    let idLastItem = lastItem.id
+                    let descriptionLastItem = lastItem.text
 
-                cy.wrap({descriptionLastItem, idLastItem})
-            }).as('lasItem')
+                    cy.log(`ID LAST ITEM: ${idLastItem}`)
+                    cy.log(`DESCRIPTION LAST ITEM: ${descriptionLastItem}`)
 
-            cy.get('@descriptionText').then((descriptionText) => {
-                cy.get('@lasItem').then((lasItem) => {
-                    const descriptionChapter = lasItem.descriptionLastItem
-                    expect(descriptionChapter.trim()).to.be.equal(descriptionText.trim())
-                    cy.CC_delete_item(lasItem.idLastItem)
+                    cy.wrap({descriptionLastItem, idLastItem})
+                }).as('lasItem')
+
+                cy.get('@descriptionText').then((descriptionText) => {
+                    cy.get('@lasItem').then((lasItem) => {
+                        const descriptionChapter = lasItem.descriptionLastItem
+                        expect(descriptionChapter.trim()).to.be.equal(descriptionText.trim())
+
+                    })
                 })
             })
         })
+    })
 
-        it(`Create item unsuccessfully with inadequate image on ${resolution}`, function () {
-            if (Cypress._.isArray(resolution)) {
-                cy.viewport(resolution[0], resolution[1])
-            } else {
-                cy.viewport(resolution)
-            }
+    context('Create item unsuccessfully', () => {
 
-            const filePath2 = 'chapter8-600px-600px.jpg'
-            let synopsisChapter8 = chance.paragraph({sentences: 1})
+        beforeEach(function () {
+            cy.intercept({
+                method: 'GET',
+                url: '**/api/items',
+            }).as('getItems')
 
-            cy.visit('/')
-            cy.wait('@getItems')
-
-            cy.get('#inputImage').attachFile(filePath2)
-            cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8)
-            cy.findByRole('button', {name: /create item/i}).should('not.be.enabled')
+            cy.intercept({
+                method: 'POST',
+                url: '**/api/items',
+            }).as('postItems')
         })
 
-        it(`Create item with file type not accepted (jpeg, bmp, svg) on ${resolution}`, function () {
-            if (Cypress._.isArray(resolution)) {
-                cy.viewport(resolution[0], resolution[1])
-            } else {
-                cy.viewport(resolution)
-            }
+        resolutions.forEach((resolution) => {
 
-            const filePath3 = 'chapter8-320px-320px.svg'
-            let synopsisChapter8 = chance.paragraph({sentences: 1})
+            it(`Create item unsuccessfully with inadequate image on ${resolution}`, function () {
+                if (Cypress._.isArray(resolution)) {
+                    cy.viewport(resolution[0], resolution[1])
+                } else {
+                    cy.viewport(resolution)
+                }
 
-            cy.visit('/')
-            cy.wait('@getItems')
+                const filePath2 = 'chapter8-600px-600px.jpg'
+                let synopsisChapter8 = chance.paragraph({sentences: 1})
 
-            cy.get('#inputImage').attachFile(filePath3)
-            cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8)
-            cy.findByRole('button', {name: /create item/i}).should('not.be.enabled')
-        })
+                cy.visit('/')
+                cy.wait('@getItems')
 
-        it(`Create item without uploading image on ${resolution}`, function () {
-            if (Cypress._.isArray(resolution)) {
-                cy.viewport(resolution[0], resolution[1])
-            } else {
-                cy.viewport(resolution)
-            }
-
-            let synopsisChapter8 = chance.paragraph({sentences: 1})
-
-            cy.visit('/')
-            cy.wait('@getItems')
-
-            cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8)
-            cy.findByRole('button', {name: /create item/i}).should('be.enabled').click()
-
-            cy.on('window:alert', (text) => {
-                expect(text).to.contains('You must to select an image')
+                cy.get('#inputImage').attachFile(filePath2)
+                cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8)
+                cy.findByRole('button', {name: /create item/i}).should('not.be.enabled')
             })
-        })
 
-        it(`Create an item with a very long description on ${resolution}`, function () {
-            if (Cypress._.isArray(resolution)) {
-                cy.viewport(resolution[0], resolution[1])
-            } else {
-                cy.viewport(resolution)
-            }
+            it(`Create item with file type not accepted (jpeg, bmp, svg) on ${resolution}`, function () {
+                if (Cypress._.isArray(resolution)) {
+                    cy.viewport(resolution[0], resolution[1])
+                } else {
+                    cy.viewport(resolution)
+                }
 
-            const filePath1 = 'chapter8-250px-250px.jpg'
-            let synopsisChapter8 = chance.paragraph({sentences: 7})
+                const filePath3 = 'chapter8-320px-320px.svg'
+                let synopsisChapter8 = chance.paragraph({sentences: 1})
 
-            cy.visit('/')
-            cy.wait('@getItems')
+                cy.visit('/')
+                cy.wait('@getItems')
 
-            cy.get('#inputImage').attachFile(filePath1)
-            cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8).invoke('val').as('descriptionText')
-            cy.get('@descriptionText').then((descriptionText) => {
-                const lengthDescriptionText = descriptionText.length
-                cy.log(`Entered text size: ${lengthDescriptionText}`)
-                expect(lengthDescriptionText, 'Entered text size:').to.be.greaterThan(300, 'Possible size')
+                cy.get('#inputImage').attachFile(filePath3)
+                cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8)
+                cy.findByRole('button', {name: /create item/i}).should('not.be.enabled')
             })
-            cy.findByRole('button', {name: /create item/i}).should('not.be.enabled')
+
+            it(`Create item without uploading image on ${resolution}`, function () {
+                if (Cypress._.isArray(resolution)) {
+                    cy.viewport(resolution[0], resolution[1])
+                } else {
+                    cy.viewport(resolution)
+                }
+
+                let synopsisChapter8 = chance.paragraph({sentences: 1})
+
+                cy.visit('/')
+                cy.wait('@getItems')
+
+                cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8)
+                cy.findByRole('button', {name: /create item/i}).should('be.enabled').click()
+
+                cy.on('window:alert', (text) => {
+                    expect(text).to.contains('You must to select an image')
+                })
+            })
+
+            it(`Create an item with a very long description on ${resolution}`, function () {
+                if (Cypress._.isArray(resolution)) {
+                    cy.viewport(resolution[0], resolution[1])
+                } else {
+                    cy.viewport(resolution)
+                }
+
+                const filePath1 = 'chapter8-250px-250px.jpg'
+                let synopsisChapter8 = chance.paragraph({sentences: 7})
+
+                cy.visit('/')
+                cy.wait('@getItems')
+
+                cy.get('#inputImage').attachFile(filePath1)
+                cy.findByPlaceholderText('Maximum allowed length: 300 characters').type(synopsisChapter8).invoke('val').as('descriptionText')
+                cy.get('@descriptionText').then((descriptionText) => {
+                    const lengthDescriptionText = descriptionText.length
+                    cy.log(`Entered text size: ${lengthDescriptionText}`)
+                    expect(lengthDescriptionText, 'Entered text size:').to.be.greaterThan(300, 'Possible size')
+                })
+                cy.findByRole('button', {name: /create item/i}).should('not.be.enabled')
+            })
         })
     })
 })
